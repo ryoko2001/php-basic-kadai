c0dcd58d149a2130bb15cdef3f921ab09331983e
ryoko2001 <ryokodesign01@gmail.com>
2026-04-03 15:09:03 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入
+    $vegetable = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    // 連想配列を出力
+    print_r($vegetable);
+    ?>
+  </p>
+</body>
+</html>
