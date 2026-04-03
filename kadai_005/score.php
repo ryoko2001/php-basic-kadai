db43a07ebdbe48935b4ee6db51336510256e3879
ryoko2001 <ryokodesign01@gmail.com>
2026-04-03 13:59:41 +0900

    kadai_005提出

---
+++ kadai_005/score.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8#>
+  <title>5章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 変数に値を代入する
+     $score1 = 80;
+     $score2 = 60;
+     $score3 = 55;
+     $score4 = 40;
+     $score5 = 100;
+     $score6 = 25;
+     $score7 = 80;
+     $score8 = 95;
+     $score9 = 30;
+     $score10 = 60;
+
+     // 全員の合計点を計算する
+     $totalscore = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+     //全員の平均点を計算する
+     $averagescore = $totalscore / 10;
+
+     //計算した平均点を表示する
+     echo $averagescore;
+     ?>
+  </p>
+</body>
+</html>
