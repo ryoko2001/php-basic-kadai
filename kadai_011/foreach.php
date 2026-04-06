8e435c1f479f5aa9018cfda69b5f755820be525e
ryoko2001 <ryokodesign01@gmail.com>
2026-04-06 14:59:40 +0900

    kadai_011再提出

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -9,16 +9,12 @@
 <body>
   <p>
     <?php
-    $vegetable = [
-      '名前' => '玉ねぎ',
-      '価格' => 200,
-      '産地' => '北海道'
-    ];
+    $vegetable = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
 
 
     // 連想配列$vegetableのキーと値を順番に出力する
     foreach ($vegetable as $key => $value) {
-      echo "{$key}: {$value} <br> " ;
+      echo "{$key}: {$value} <br>" ;
     }
 
     ?>
